f33536abfb71d631cc413f1c755e5ae72e570e35
huxiaoyou <huxiaoyou888@gmail.com>
2024-12-23 21:54:58 +0800

    fix 初始化

---
+++ app/src/main/java/com/jsmiao/webapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.jsmiao.webapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.jsmiao.webapp.controls.MWebView;
+
+public class MainActivity extends AppCompatActivity {
+    private MWebView mWebView; // webView 控件
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 设置页面全屏，隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 获取控件
+        mWebView = (MWebView) findViewById(R.id.mWebView);
+        mWebView.setActivity(this);
+
+        String url;
+        // 设置url地址
+        url = "https://wap.agtyapp.com";
+
+        // 配置 WebView
+        setupWebView();
+
+        // 加载url
+        mWebView.loadUrl(url);
+    }
+
+    private void setupWebView() {
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("http") || url.startsWith("https")) {
+                    // 普通网页加载
+                    return false;
+                } else {
+                    // 自定义协议处理
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        // 如果没有安装相应的应用
+                        Toast.makeText(MainActivity.this, "无法打开该链接，请检查是否安装了相应的应用", Toast.LENGTH_SHORT).show();
+                    }
+                    return true;
+                }
+            }
+        });
+    }
+
+    /*
+     * 接管返回键
+     */
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
